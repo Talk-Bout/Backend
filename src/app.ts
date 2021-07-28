ac7ebee92f24a3d79d56d2d7d6d961fe9e5f31a9
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-07-28 22:44:18 +0900

    add User -role

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 dotenv.config()
 
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient()
 
 const app = express()
