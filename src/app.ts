eeb2506b6436e519a2577b0c6c46942d5835de30
ombreman <ombreman21@gmail.com>
2021-07-29 02:39:48 +0100

    feat: add apis

--- src/app.ts
+++ src/app.ts
@@ -14,37 +14,34 @@ app.use(cors({ credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// 게시글 전체 불러오기
-app.get('/api/posts', async (req: Request, res: Response) => {
-    const posts = await prisma.post.findMany()
-    res.json(posts)
+// 글 상세보기
+app.get('/posts/:postId', async (req: Request, res: Response) => {
+    const id = req.params.postId
+    const postDetail = await prisma.post.findUnique({
+        where: { postId: Number(id) }
+    })
+    res.json(postDetail)
 })
 
-// 내가 쓴 글 불러오기 >> 작성 중
-// app.get('/api/posts/:userId', (req: Request, res: Response) => {
-//     // 아이디 입력값 받기
-//     const postId = req.params.id
-//     // 입력받은 아이디에 해당하는 게시글 찾아 불러옴
-
-//     // 찾은 게시글들 보냄
-//     res.json(postId)
-// })
-
-// 글 상세보기 >> 미완성 >> 불러올 내용에 좋아요 likes 추가 필요
-// app.get('/api/posts/:postId', async (req: Request, res: Response) => {
-//     // 어떤 게시글을 찾아줄지 요청 받음
-//     const postId = req.params.id
-//     console.log(postId)
-//     // client가 찾는 게시글 찾아서 불러옴
-//     const postDetail = await prisma.post.findUnique({
-//         where: { id: Number(postId) }
-//     })
-//     // 찾은 게시글 정보 보냄
-//     res.json(postDetail)
-// })
+// 내가 쓴 글 불러오기 & 카테고리별 게시글 불러오기 
+app.get('/posts', async (req: Request, res: Response) => {
+    const category = req.query.category
+    if (req.query.nickname) {
+        const user = req.query.nickname
+        const postsByUser = await prisma.post.findMany({
+            where: { nickname: String(user) }
+        })
+        res.json(postsByUser)
+    } else if (req.query.category) {
+        const postsByCategory = await prisma.post.findMany({
+            where: { category: String(category) }
+        })
+        res.json(postsByCategory)
+    }
+})
 
 // 게시글 작성하기
-app.post('/api/posts', async (req: Request, res: Response) => {
+app.post('/posts', async (req: Request, res: Response) => {
     const { nickname, title, content, category } = req.body
     const post = await prisma.post.create({
         data: {
@@ -59,7 +56,7 @@ app.post('/api/posts', async (req: Request, res: Response) => {
 })
 
 // 게시글 수정하기
-app.patch('/api/posts/:postId', async (req: Request, res: Response) => {
+app.patch('/posts/:postId', async (req: Request, res: Response) => {
     const { title, content } = req.body
     const id = req.params.postId
     const updatedPost = await prisma.post.update({
@@ -74,14 +71,37 @@ app.patch('/api/posts/:postId', async (req: Request, res: Response) => {
 })
 
 // 게시글 삭제하기
-app.delete('/api/posts/:postId', async (req: Request, res: Response) => {
+app.delete('/posts/:postId', async (req: Request, res: Response) => {
     const id = req.params.postId
-    console.log('삭제할 게시글 번호: ' + id)
+    console.log(`삭제할 게시글 번호: ${id}`)
     const deletePost = await prisma.post.delete({
         where: { postId: Number(id) }
     })
     console.log('삭제완료!')
     res.json(deletePost)
+})
+
+// 북마크 추가하기
+app.post('/users/:postId/bookmark', async (req: Request, res: Response) => {
+    const post = req.params.postId
+    const user = req.body.nickname
+    const bookmark = await prisma.bookmark.create({
+        data: {
+            postId: Number(post),
+            nickname: user
+        }
+    })
+    res.json(bookmark)
+})
+
+// 북마크 불러오기
+app.get('/users/:nickname/bookmark', async (req: Request, res: Response) => {
+    const nickname = req.params.nickname
+    console.log(nickname)
+    const bookMarkByNickname = await prisma.bookmark.findMany({
+        where: { nickname: nickname }
+    })
+    res.json(bookMarkByNickname)
 })
 
 app.listen(PORT, () => console.log('Server is running on port', PORT))
