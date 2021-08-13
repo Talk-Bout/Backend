f64d25adedb9179de97522c069b3f01681a0119e
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-08-14 02:37:30 +0900

    fix : delete uselss console log

--- src/User/services/read.myBootcampBookmarks.ts
+++ src/User/services/read.myBootcampBookmarks.ts
@@ -1,7 +1,39 @@
 import { prisma } from '../../Infrastructures/utils/prisma'
 import { NicknameValidator } from '../validators'
 
-export default (nickname: NicknameValidator) => {
+export default async (nickname: NicknameValidator) => {
   const BootcampBookmark = prisma.bootcampBookmark
-  return BootcampBookmark.findMany({ where: nickname })
+  const Bootcamp = prisma.bootcamp
+
+  let result: any = []
+  const bookmarks = await BootcampBookmark.findMany({ where: nickname })
+
+  for (let bookmark of bookmarks){
+ 
+    let bootcampName = bookmark.bootcampName
+    let bootcamps = await Bootcamp.findFirst({ where: {bootcampName: bootcampName },include: { review: true,}})
+   
+    
+  }
+
+  for (let bootcamp of result){
+    let totalReviewNumber = bootcamp?.review.length
+    let stars = 0
+    let reviews = bootcamp.review
+
+    if(reviews.length !== 0) {
+      for(let i =0; i < reviews.length; i++)
+      (stars += reviews[i].stars)}
+
+    //console.log(stars)
+    let avgStar = stars/totalReviewNumber
+    //console.log(avgStar)
+ 
+    if (avgStar){
+      bootcamp["avgStar"] = avgStar
+    }
+  }
+ 
+  return result
 }
+
